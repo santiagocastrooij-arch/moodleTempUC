3f01462382f4f2644f610ff48541de1aecd46935
santiagocastrooij-arch <santiago.castro.oij@alumni.immune.institute>
2026-05-11 17:00:17 -0500

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -3,6 +3,10 @@ export const estudiantes = [
         nombre: "Santiago Gaviria",
         github: "ChicoGavi",
         frase: "¡Bienvenidos al proyecto colaborativo!",
+    },  {
+        nombre: "Santiago dasdsadasdas",
+        github: "santiagocastrooij-arch",
+        frase: "¡Bienvenidos al proyecto dadadasa!",
     }
 
     // Continuando con el formato anterior, realizar cada uno su perfil y hacer un PR.
